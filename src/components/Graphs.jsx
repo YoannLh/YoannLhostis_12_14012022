5f767c52bdf20a7f6b373d2b4033714d2dbdef7f
L'hostis <yolhostis@live.fr>
2022-01-15 01:44:16 +0100

    Ajout composant Barcharts

--- src/components/Graphs.jsx
+++ src/components/Graphs.jsx
@@ -1,15 +1,21 @@
 import styled from "styled-components"
+import BarCharts from "./BarCharts"
 
 const StyledGraphs = styled.div`
     display: flex;
     flex-direction: column;
+    width: 75%;
+`
+
+const StyledBarCharts = styled(BarCharts)`
+    background: grey;
 `
 
 
 function Graphs() {
     return(
         <StyledGraphs>
-            graphs
+            <StyledBarCharts />
         </StyledGraphs>
     )
 }
