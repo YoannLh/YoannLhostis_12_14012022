54bb265bacb1c787589bb260cf65375ddb8c7947
L'hostis <yolhostis@live.fr>
2022-01-15 02:01:24 +0100

    Ajout des composants Line, Radio et Pie bruts

--- src/components/Graphs.jsx
+++ src/components/Graphs.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components"
 import BarCharts from "./BarCharts"
+import LineCharts from './LineCharts'
+import RadarCharts from "./RadarCharts"
+import PieCharts from "./PieCharts"
 
 const StyledGraphs = styled.div`
     display: flex;
@@ -16,6 +19,11 @@ function Graphs() {
     return(
         <StyledGraphs>
             <StyledBarCharts />
+            <div style={{"display": "flex", "width": "100%", "height": "50%", "background": "black"}}>
+                <LineCharts />
+                <RadarCharts />
+                <PieCharts />
+            </div>
         </StyledGraphs>
     )
 }
